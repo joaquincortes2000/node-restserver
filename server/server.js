0f8bfb47fae9553081d14e20b24419d2ca41741d
Joaquin <joaquincorteskha@gmail.com>
2019-06-14 02:33:53 -0400

    RestServer - usuario

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 require('./config/config');
 
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
+
+const app = express();
 
 const bodyParser = require('body-parser');
 
@@ -13,43 +15,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
+app.use(require('./routes/usuario'));
 
 
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
 
-app.get('/usuario', function(req, res) {
-    res.json('getUsuario')
+    if (err) throw err;
+    console.log('Base de datos ONLINE');
+
 });
-
-//Crear nuevos registros
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: "El nombre es necesario"
-        });
-    } else {
-        res.json({
-            body
-        });
-    }
-});
-
-//Actualizar data
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-});
-
-//Borra las tablas
-app.delete('/usuario', function(req, res) {
-    res.json('delete Usuario')
-});
-
-
 
 
 app.listen(process.env.PORT, () => {

ab4d4ece81a90b0bb5d863327e873e9fc2851d5d
Joaquin <joaquincorteskha@gmail.com>
2019-06-14 02:49:19 -0400

    Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://joaquincortes:1ekjIyrutwra4pOH@cluster0-epuwb.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
